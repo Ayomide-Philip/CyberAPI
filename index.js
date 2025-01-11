73b46de4f62fe98e0f044b55406728dcf0226b6d
A-pilip <areoayomide2008@gmail.com>
2025-01-12 00:27:42 +0100

    add nmap scan functionality to /nmapScan endpoint

--- index.js
+++ index.js
@@ -114,7 +114,19 @@ app.get("/dnsLookup", (req, res) => {
 });
 
 app.get("/nmapScan", (req, res) => {
- 
+ const opts = {
+  range: ['scanme.nmap.org', '192.168.0.0/26']
+};
+
+
+nmap.scan(opts, function(err, report) {
+  if (err) throw new Error(err);
+
+  for (let item in report) {
+    console.log(JSON.stringify(report[item], null, 2));
+  }
+});
+
 });
 
 app.listen(port, () => {
