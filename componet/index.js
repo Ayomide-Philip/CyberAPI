4dc64c6db11820d26dd69604bca0b2ce39d25677
A-pilip <areoayomide2008@gmail.com>
2025-01-18 16:36:57 +0100

    feat: add pill data for IP-related features and DNS lookups

---
+++ componet/index.js
@@ -0,0 +1,24 @@
+var pills = [
+  {
+    id: 1,
+    pill: "My Ip Address",
+  },
+  {
+    id: 2,
+    pill: "Ip Location",
+  },
+  {
+    id: 3,
+    pill: "Reverese DNS Lookup",
+  },
+  {
+    id: 4,
+    pill: "DNS Lookup",
+  },
+  {
+    id: 5,
+    pill: "Is Port Reachable",
+  },
+];
+
+export default pills;
